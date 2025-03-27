e8cf1e911d273dcf81c4571fe0c0d8f9dec4d651
Justin Cooke <justin.a.cooke@gmail.com>
2025-03-26 23:37:11 -0400

    integrating Camila's expanded base game, with destroyable bricks, into the restructured code from Ann

--- smashCoreGame/paddle.py
+++ smashCoreGame/paddle.py
@@ -24,25 +24,9 @@ class Paddle(pygame.sprite.Sprite):
         # Fetch the rectangle object that has the dimensions of the image.
         self.rect = self.image.get_rect()
 
-    def move_left(self, pixels):
-        """ Move paddle left by pixels
-            Check that the paddle is not going too far (off the screen)
-        """
-        self.rect.x -= pixels
-        if self.rect.x < 0:
-            self.rect.x = 0
-
-    def move_right(self, pixels):
-        """ Move paddle right by pixels
-            Check that the paddle is not going too far (off the screen)
-        """
-        self.rect.x += pixels
-        if self.rect.x > settings.WIDTH - settings.PAD_WIDTH:
-            self.rect.x = settings.WIDTH - settings.PAD_WIDTH
-
     def move_by_mouse(self, mouse_position):
         self.rect.centerx = mouse_position
-        if self.rect.x < 0:
-            self.rect.x = 0
-        if self.rect.x > settings.WIDTH - settings.PAD_WIDTH:
-            self.rect.x = settings.WIDTH - settings.PAD_WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > settings.WIDTH:
+            self.rect.right = settings.WIDTH
