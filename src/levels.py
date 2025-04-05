21a1885a80f8704bf8c7c5a371d7ef9ea4a64e9a
RoxoCam <becker.camila@gmail.com>
2025-04-05 03:56:23 +0100

    Update levels.py
    added specific brick colors

--- src/levels.py
+++ src/levels.py
@@ -4,33 +4,29 @@
 """
 
 import pygame
-from random import randrange as rnd
 from enum import Enum, auto
 
 from src.brick import Brick
+import constants # import the constants file.
 
 
 class Levels:
 
-    # enum to hold all level names
     class LevelName(Enum):
-        SMASHCORE_1 = auto() # first SMASHCORE level
-
-
+        SMASHCORE_1 = auto()
 
     def __init__(self):
         pass
 
-    # this adds the specified level to the passed GameWorld
     @staticmethod
     def build_level(gw, level_name):
 
         match level_name:
             case Levels.LevelName.SMASHCORE_1:
+                colors = [constants.RED, constants.ORANGE, constants.GREEN, constants.YELLOW, constants.LIGHTBLUE] # define the colors
                 for i in range(10):
-                    for j in range(4):
+                    for j in range(5): # change to 5 rows to use all the colors.
                         gw.world_objects.append(Brick(pygame.Rect(10 + 120 * i, 60 + 70 * j, 100, 50),
-                                          (rnd(30, 256), rnd(30, 256), rnd(30, 256))))
+                                          colors[j]))
             case _:
                 pass
-
