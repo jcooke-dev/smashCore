cc7c8bbef1b7da864276d22f1f0d2d5a630544a4
RoxoCam <becker.camila@gmail.com>
2025-04-06 19:45:10 +0100

    Added menu and credit

--- src/gamestates.py
+++ src/gamestates.py
@@ -4,7 +4,9 @@ from enum import Enum, auto
 
 class GameStates(Enum):
     SPLASH = auto()
+    MENU_SCREEN = auto()
     READY_TO_LAUNCH = auto()
     PLAYING = auto()
     PAUSED = auto()
     GAME_OVER = auto()
+    CREDITS = auto()
