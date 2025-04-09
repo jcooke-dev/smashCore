cea2f1d646a48fe77f4fc1e3b47f8ddcfa142fe1
Rex Vargas <162274638+wreckx@users.noreply.github.com>
2025-04-09 14:56:54 -0400

    Adds type annotation to motionmodels and playerstate module

--- src/motionmodels.py
+++ src/motionmodels.py
@@ -16,9 +16,9 @@ class MotionModels(Enum):
     """ Enum with all possible MotionModel values """
 
     # initial motion code with simplified calculations and clock.tick(fps) to time the update loop
-    SIMPLE_1 = auto()
+    SIMPLE_1: Enum = auto()
 
     # motion calculations using POS/VEL/ACC vectors and the clock.tick() returned delta time, so the motion is unlinked
     # from the frame rate - can set a MAX_FPS_VECTOR to limit the fps a bit (rather than letting it run all out)
-    VECTOR_1 = auto()
+    VECTOR_1: Enum = auto()
 
