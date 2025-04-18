9cd1bdd89f063432ad4596683e8c40f8c8961bf6
Ann <arauscher@gmail.com>
2025-04-18 16:19:38 -0400

    Create leaderboard test.

--- src/leaderboard.py
+++ src/leaderboard.py
@@ -35,16 +35,23 @@ class Leaderboard:
         persistence.store_object(self, path)
 
     def is_high_score(self, score: int) -> bool:
-        return (score >= min(scr.score for scr in self.l_top_scores)) if len(self.l_top_scores) >= constants.LEADERBOARD_SIZE else True
+        return (score > min(scr.score for scr in self.l_top_scores)) if len(self.l_top_scores) >= constants.LEADERBOARD_SIZE else True
 
     def add_score(self, ps, ui):
-
+        """
+        Called after it is determined player has achieved a high score.
+        Adds player's high score to the leaderboard
+        If leaderboard is full, bumps a player off and resorts.
+        :param ps: PlayerState
+        :param ui: UserInterface
+        :return:
+        """
         score = Score(ps.score, ps.level, ui.tb_initials_text)
 
         if len(self.l_top_scores) < constants.LEADERBOARD_SIZE:
             self.l_top_scores.append(score)
         else:
-            # full, so remove lowest score
+            # full, so remove lowest scor
             self.l_top_scores[0] = score
 
         # reset the ui var holding the entered initials
--- tests/test_leaderboard.py
+++ tests/test_leaderboard.py
@@ -9,59 +9,99 @@
     Module Description: This is the test harness for the Leaderbaord class.
 """
 import pytest
-
+from unittest import mock
 from score import Score
-from src import Leaderboard
+from src import Leaderboard, PlayerState, UserInterface
 
 
 @pytest.fixture
-def score_list():
-    score_list: list[Score] = [Score(500, 1, "aaa"),
-                           Score(300, 1, "bbb"),
-                           Score(100, 1, "ccc"),
-                           Score(50, 1, "ddd")]
-    return score_list
-
-@pytest.fixture
-def score_list_full():
-    score_list: list[Score] = [Score(500, 1, "aaa"),
-                               Score(300, 1, "bbb"),
+def leaderboard_partial():
+    score_list: list[Score] = [Score(50, 1, "ddd"),
                                Score(100, 1, "ccc"),
-                               Score(50, 1, "ddd"),
-                               Score(350, 1, "eee"),
-                               Score(250, 1, "fff"),
-                               Score(150, 1, "aaa"),
-                               Score(550, 1, "ddd"),
-                               Score(560, 1, "ggg"),
-                               Score(120, 1, "hhh")
+                               Score(300, 1, "bbb"),
+                               Score(500, 1, "aaa")
                                ]
-    return score_list
+    leaderboard = Leaderboard()
+    leaderboard.l_top_scores = score_list
+    return leaderboard
 
-def test_is_high_score(score_list):
-    top_scores = Leaderboard()
-    top_scores.l_top_scores = score_list
-    assert top_scores.is_high_score(1000)
+@pytest.fixture
+def leaderboard_full():
+    score_list: list[Score] = [
+        Score(50, 1, "iii"),
+        Score(100, 1, "ccc"),
+        Score(120, 1, "hhh"),
+        Score(150, 1, "aaa"),
+        Score(250, 1, "fff"),
+        Score(300, 1, "bbb"),
+        Score(350, 1, "eee"),
+        Score(500, 1, "aaa"),
+        Score(550, 1, "ddd"),
+        Score(560, 1, "ggg")
+    ]
+    leaderboard = Leaderboard()
+    leaderboard.l_top_scores = score_list
+    return leaderboard
+
+def test_is_high_score(leaderboard_partial):
+    assert leaderboard_partial.is_high_score(1000)
 
 
-def test_is_high_score_without_full_list(score_list):
-    top_scores = Leaderboard()
-    top_scores.l_top_scores = score_list
-    assert top_scores.is_high_score(30)
+def test_is_high_score_without_full_list(leaderboard_partial):
+    assert leaderboard_partial.is_high_score(30)
+
+def test_is_high_score_without_full_list_same_low_score(leaderboard_partial):
+    assert leaderboard_partial.is_high_score(50)
+
+def test_is_not_high_score_with_full_list(leaderboard_full):
+    assert leaderboard_full.is_high_score(30) is False
 
 
-def test_is_not_high_score_with_full_list(score_list_full):
-    top_scores = Leaderboard()
-    top_scores.l_top_scores = score_list_full
-    assert top_scores.is_high_score(30) is False
+def test_is_not_high_score_with_full_list_same_min_score(leaderboard_full):
+    assert leaderboard_full.is_high_score(50) is False
 
 
-def test_is_not_high_score_with_full_list_same_min_score(score_list_full):
-    top_scores = Leaderboard()
-    top_scores.l_top_scores = score_list_full
-    assert top_scores.is_high_score(50) is False
+def test_is_high_score_with_full_list(leaderboard_full):
+    assert leaderboard_full.is_high_score(400)
 
 
-def test_is_high_score_with_full_list(score_list_full):
-    top_scores = Leaderboard()
-    top_scores.l_top_scores = score_list_full
-    assert top_scores.is_high_score(400)
+def test_add_score_to_parial_list(leaderboard_partial):
+    ps = PlayerState()
+    ps.score = 400
+    ps.level = 2
+    ps.lives = 0
+    ui = mock.Mock()
+    ui.tb_initials_text = "abc"
+    leaderboard_partial.add_score(ps, ui)
+
+    expected_list: list[Score] = [
+                                  Score(50, 1, "ddd"),
+                                  Score(100, 1, "ccc"),
+                                  Score(300, 1, "bbb"),
+                                  Score(400, 2, "abc"),
+                                  Score(500, 1, "aaa")]
+    assert leaderboard_partial.l_top_scores == expected_list
+
+
+def test_add_score_to_full_list(leaderboard_full):
+    ps = PlayerState()
+    ps.score = 400
+    ps.level = 2
+    ps.lives = 0
+    ui = mock.Mock()
+    ui.tb_initials_text = "xyz"
+    leaderboard_full.add_score(ps, ui)
+    expected_list: list[Score] = [
+        Score(100, 1, "ccc"),
+        Score(120, 1, "hhh"),
+        Score(150, 1, "aaa"),
+        Score(250, 1, "fff"),
+        Score(300, 1, "bbb"),
+        Score(350, 1, "eee"),
+        Score(400, 2, "xyz"),
+        Score(500, 1, "aaa"),
+        Score(550, 1, "ddd"),
+        Score(560, 1, "ggg")
+    ]
+    assert leaderboard_full.l_top_scores == expected_list
+
