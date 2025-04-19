f8b914533b754b4ec046e16ab44599ae2c25e7e2
Ann <arauscher@gmail.com>
2025-04-19 14:22:48 -0400

    minor refactoring

--- tests/test_worldobject.py
+++ tests/test_worldobject.py
@@ -15,9 +15,6 @@ def test_default_initialization(wo):
     assert wo.can_react is False
     assert wo.primed_collision is True
 
-def test_should_score(wo):
-    assert wo.should_score() is False
-
 def test_should_remove(wo):
     assert wo.should_remove() is False
 
