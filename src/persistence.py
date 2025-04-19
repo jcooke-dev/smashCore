d91ed724d704df7f9741b281511b60979ef54358
Justin Cooke <justin.a.cooke@gmail.com>
2025-04-19 13:11:01 -0400

    cleaning up debugging statements

--- src/persistence.py
+++ src/persistence.py
@@ -16,40 +16,27 @@ import platform
 import constants
 
 # paths to serialized files
-GAME_DATA_PATH: str = None
+GAME_DATA_PATH: str = None # holds the proper OS-dependent user settings dir
 APP_DATA_PATH_WINDOWS: str = 'AppData\\Local\\'
-# APP_DATA_PATH_MAC = 'AppData\\Local\\'
-# APP_DATA_PATH_LINUX = 'AppData\\Local\\'
 LEADERBOARD_FILENAME: str = 'leaderboard.pkl'
 
 
 def find_game_data_path():
     global GAME_DATA_PATH
-    # GAME_DATA_PATH = 'settings/'
-
-    print(platform.platform())
-    print(platform.system())
-    print(os.path.expanduser('~'))
 
     op_sys: str = platform.system()
-    user_profile_path: str = os.path.expanduser('~') #os.environ['USERPROFILE']
+    user_profile_path: str = os.path.expanduser('~')
 
     # OS determines best appData path to game settings
     if 'windows' in op_sys.lower():
         GAME_DATA_PATH = os.path.join(user_profile_path, APP_DATA_PATH_WINDOWS, constants.GAME_NAME)
     elif 'darwin' in op_sys.lower(): # macOS
-        # GAME_DATA_PATH = os.path.join(user_profile_path, APP_DATA_PATH_MAC, constants.GAME_NAME)
         GAME_DATA_PATH = os.path.join(user_profile_path, '.' + constants.GAME_NAME)
     elif 'linux' in op_sys.lower():
-        # GAME_DATA_PATH = os.path.join(user_profile_path, APP_DATA_PATH_LINUX, constants.GAME_NAME)
         GAME_DATA_PATH = os.path.join(user_profile_path, '.' + constants.GAME_NAME)
     else:
         # default to a game-named settings dir in the working directory
         GAME_DATA_PATH = os.path.join(constants.GAME_NAME + '_settings/')
-
-    print(GAME_DATA_PATH)
-
-
 
 def store_object(obj: object, filename: str):
 
