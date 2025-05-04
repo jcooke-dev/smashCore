0775b1d01fbb2463df52e78b4b68f1876f231037
Ann Rauscher <arauscher@gmail.com>
2025-05-03 22:53:56 -0400

    Update test_gamesettings.py

--- tests/test_gamesettings.py
+++ tests/test_gamesettings.py
@@ -1,3 +1,13 @@
+"""
+    Project: SmashCore
+    Course: UMGC CMSC 495 (7383)
+    Term: Spring 2025
+    Date: 20250401
+    Code Repository: https://github.com/jcooke-dev/smashCore
+    Authors: Justin Cooke, Ann Rauscher, Camila Roxo, Justin Smith, Rex Vargas
+    Module Description: This is the test harness for testing the GameSettings.
+"""
+
 import pytest
 from unittest.mock import patch, MagicMock
 from gamesettings import GameSettings
